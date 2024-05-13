881c4f626a8759e95f1d3643fe804473ab7e75be
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-05-13 15:53:21 -0300

    Set up react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Recursive } from 'next/font/google';
 import NextTopLoader from 'nextjs-toploader';
+
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Providers from '@/components/Providers';
 import { Toaster } from '@/components/ui/toaster';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Recursive({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'caseCobra',
@@ -23,8 +25,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <NextTopLoader color='#16a34a' />
         <Navbar />
-        <main className='flex flex-col min-h-[calc(100vh-3.5rem-1px)]'>
-          <div className='flex-1 flex flex-col h-full'>{children}</div>
+        <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
+          <div className='flex-1 flex flex-col h-full'>
+            <Providers>{children}</Providers>
+          </div>
           <Footer />
         </main>
         <Toaster />
